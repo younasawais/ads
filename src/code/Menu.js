bd6e555f7f54960472dd53b62d5e84b43a46eb83
Younas Awais <younasawais@gmail.com>
2020-07-14 01:56:21 -0400

    navLinks created, cleanup Article needed

--- src/code/Menu.js
+++ src/code/Menu.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import {Redirect, NavLink, Route, Switch} from 'react-router-dom';
 import TestComp1 from './components/TestComp1';
 import TestComp2 from './components/TestComp2';
+import Article from './components/Article';
 
 
 class Menu extends Component {
@@ -24,10 +25,45 @@ class Menu extends Component {
         console.log(this.props);
         const {props} = this;
         return (
-            <div>
-                <NavLink to='/testcomp1'>TestComp1</NavLink>
-                <NavLink to='/testcomp2'>TestComp2</NavLink>
+            <Fragment>            
+            <section className="menu cid-s4lUbDTTEI" once="menu" id="menu2-3">
+            <nav className="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm bg-color transparent">
+              <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <div className="hamburger">
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                  </div>
+              </button>
+              <div className="menu-logo">
+                  <div className="navbar-brand">
+                      <span className="navbar-logo">
+                          <a href="https://qouh.com">
+                              <img src="assets/images/logo2.png" alt="Qouh" title="Qouh" style={{height: '3.8rem'}}/> 
+                          </a>
+                      </span>
+                      
+                  </div>
+              </div>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul className="navbar-nav nav-dropdown" data-app-modern-menu="true"><NavLink to='/article' className="nav-item dropdown">
+                          <a className="nav-link link text-black dropdown-toggle display-4" href="https://mobirise.co" data-toggle="dropdown-submenu" aria-expanded="false">
+                              Subtopic1</a><div className="dropdown-menu"><a className="text-black dropdown-item display-4" href="https://mobirise.co" aria-expanded="false">New Item</a></div>
+                      </NavLink>
+                    <NavLink to='/article' className="nav-item"><a className="nav-link link text-black display-4" href="https://mobirise.co">Subtopic2</a></NavLink>
+                    <NavLink to='/article' className="nav-item"><a className="nav-link link text-black display-4" href="https://mobirise.co">Subtopic3</a></NavLink>
+                    <NavLink to='/article' className="nav-item"><a className="nav-link link text-black display-4" href="https://mobirise.co">Subtopic4</a></NavLink>
+                    <NavLink to='/testcomp1' className="nav-item"> <a className="nav-link link text-black display-4" href="https://mobirise.co">Subtopic5</a></NavLink>
+                    </ul>
+                      </div>
+                  </nav>
+              </section>
                 <Switch>
+                    <Route
+                        path='/article'
+                        render={({location, match}) => <Article {...props}/>}    
+                    />
                     <Route
                         path='/testcomp1'
                         render={({location, match}) => <TestComp1 {...props}/>}    
@@ -41,7 +77,7 @@ class Menu extends Component {
                 <p>Menu component</p>
                 <button onClick={this.changeVal}>Hello</button>
                 <p>Current Value : {this.props.article.articleId} </p>
-            </div>
+            </Fragment>
         );
     }
 }
