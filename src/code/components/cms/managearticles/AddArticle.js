6771078428549336b169f07739b60301d9e2d1e2
Younas Awais <younasawais@gmail.com>
2020-07-21 00:05:31 -0400

    All elements added to AddArticle, next cleanup

--- src/code/components/cms/managearticles/AddArticle.js
+++ src/code/components/cms/managearticles/AddArticle.js
@@ -12,8 +12,83 @@ class AddArticles extends Component {
                     <button type="button" class="btn btn-warning">Close</button>
                 </div>
                 <div>
-                    <h3>Title :</h3>
+                    <p>Title :</p>
                     <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                    <p>Item name inside menu:</p>
+                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                </div>
+                <div>
+                    <div className='createNewMenu'>
+                        <p>Create new Menu?</p>
+                        <input type="checkbox"/>
+                        <p>Menu Name</p>
+                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                        <p>Menu item name</p>
+                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                        <p>Add as a subitem?</p>
+                        <input type="checkbox"/>
+                        <p>Menu subItem name</p>
+                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                    </div>
+                    <p>Add item to menu</p>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown button
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+                    <p>Add as a subItem?</p>
+                    <input type="checkbox"/>
+                    <p>Menu item name for adding sub item</p>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown button
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+                    
+                    <p>Link ID</p>
+                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                    <p>Tags</p>
+                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                    <p>Reference</p>
+                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                    <p>Active?</p>
+                    <input type="checkbox"/>                    
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Text 1</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>                   
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Text 2</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Upload</span>
+                        </div>
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="inputGroupFile01"/>
+                            <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Upload</span>
+                        </div>
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="inputGroupFile01"/>
+                            <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                        </div>
+                    </div>
                 </div>
             </Fragment>
         );
